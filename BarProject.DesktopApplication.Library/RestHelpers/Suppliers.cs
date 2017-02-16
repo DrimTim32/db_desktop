c96e6f7a3e6f789ac3ce8c67e5b9d9aed1dd0f3c
MaLiN2223 <szyszka45@gmail.com>
2017-02-17 00:59:37 +0100

    suppliers for application

---
+++ BarProject.DesktopApplication.Library/RestHelpers/Suppliers.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarProject.DesktopApplication.Library.RestHelpers
+{
+    using DatabaseProxy.Models.ReadModels;
+    using RestSharp;
+    using RestSharp.Serializers;
+
+    public partial class RestClient
+    {
+        public async Task<IRestResponse<List<ShowableSupplier>>> GetSuppliers()
+        {
+            var request = new RestRequest("api/suppliers/", Method.GET);
+            request.AddHeader("Authorization", $"bearer {token}");
+            request.AddHeader("Content-Type", "application/json");
+            var data = await client.ExecuteGetTaskAsync<List<ShowableSupplier>>(request);
+            return data;
+        }
+        public void RemoveSuppliers(int? id, Action<IRestResponse, RestRequestAsyncHandle> callback)
+        {
+            var request = new RestRequest($"api/suppliers/{id}", Method.DELETE);
+            request.AddHeader("Authorization", $"bearer {token}");
+            request.AddHeader("Content-Type", "application/json");
+            client.ExecuteAsync(request, callback);
+        }
+        public void AddSuppliers(ShowableSupplier category, Action<IRestResponse, RestRequestAsyncHandle> callback)
+        {
+            var request = new RestRequest("api/suppliers/", Method.POST) { RequestFormat = DataFormat.Json };
+            request.AddHeader("Authorization", $"bearer {token}");
+            request.JsonSerializer = new JsonSerializer();
+            request.AddBody(category);
+            client.ExecuteAsync(request, callback);
+        }
+        public void UpdateSuppliers(ShowableSupplier category, Action<IRestResponse, RestRequestAsyncHandle> callback)
+        {
+            var request = new RestRequest($"api/suppliers/{category.Id}", Method.PATCH) { RequestFormat = DataFormat.Json };
+            request.AddHeader("Authorization", $"bearer {token}");
+            request.JsonSerializer = new JsonSerializer();
+            request.AddBody(category);
+            client.ExecuteAsync(request, callback);
+        }
+    }
+}
